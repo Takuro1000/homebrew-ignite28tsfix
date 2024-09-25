5d8eed5c7482562231024d291c00cea3bd0936ed
Takuro1000 <taku.0116.19@gmail.com>
2024-09-25 16:05:14 +0900

    fix desc

--- ignite.rb
+++ ignite.rb
@@ -1,5 +1,5 @@
 class Ignite < Formula
-  desc "Build, launch, and maintain any crypto application with Ignite CLI"
+  desc "Takuro1000 ignite ts test"
   homepage "https://github.com/ignite/cli"
   url "https://github.com/Takuro1000/ignite28tsfix/archive/refs/tags/v28.5.3ts.tar.gz" # 最新のバージョンを指定
   sha256 "cb84ed7bb65c3e13f3f867f2ae98c317272f24a05f27eb88ad4cfd7212de0e34" # 正しいSHA256ハッシュを指定
